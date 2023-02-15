01574beb0ae0005418d1634c0f697384647197f0
Aseer KT <aseer@wednesday.is>
2023-02-15 11:32:12 +0530

    chore: restructure directories and files

--- scripts/setup-local.sh
+++ scripts/setup-local.sh
@@ -6,11 +6,11 @@ eval $(minikube -p minikube docker-env)
 
 docker build . -t aseerkt/k8-express
 
-kubectl apply -f config/k8/mongo.secret.yaml
-kubectl apply -f config/k8/mongo.configmap.yaml
+kubectl apply -f k8/mongo.secret.yaml
+kubectl apply -f k8/mongo.configmap.yaml
 
-kubectl apply -f config/k8/mongo.yaml
-kubectl apply -f config/k8/express-app.yaml
+kubectl apply -f k8/mongo.yaml
+kubectl apply -f k8/express-app.yaml
 
 echo "Waiting for deployment/services to execute..."
 sleep 15
