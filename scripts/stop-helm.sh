17505f57bb10a5de8ff1af7dc0e6d49826f8218c
Aseer KT <aseer@wednesday.is>
2023-02-15 11:28:33 +0530

    chore: update stop helm script

--- scripts/stop-helm.sh
+++ scripts/stop-helm.sh
@@ -1,6 +1,9 @@
 RELEASE=mongo-express
 
-helm uninstall $RELEASE
+helm delete $RELEASE
+
+echo "Waiting for chart deployments/services to terminate"
+sleep 10
 
 # Remove docker image
 minikube image rm aseerkt/k8-express
